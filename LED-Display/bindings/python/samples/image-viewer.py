f36fe2b1c011629139481d0ce0916bbb58cf7845
landonhellmandpea <landonhellman@dpengineering.org>
2023-02-15 15:32:14 -0800

    got githubbed. main.py should work

--- LED-Display/bindings/python/samples/image-viewer.py
+++ LED-Display/bindings/python/samples/image-viewer.py
@@ -4,11 +4,13 @@ import sys
 
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image
-from rgbmatrix import graphics
 
+if len(sys.argv) < 2:
+    sys.exit("Require an image argument")
+else:
+    image_file = sys.argv[1]
 
-
-
+image = Image.open(image_file)
 
 # Configuration for the matrix
 options = RGBMatrixOptions()
@@ -16,7 +18,7 @@ options.cols = 64
 options.rows = 32
 options.chain_length = 4
 options.parallel = 1
-options.gpio_slowdown = 2
+options.gpio_slowdown = 4
 options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
 
 matrix = RGBMatrix(options = options)
@@ -24,7 +26,7 @@ matrix = RGBMatrix(options = options)
 # Make image fit our screen.
 image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
 
-matrix.SetImage(image.convert('RGB'), -5)
+matrix.SetImage(image.convert('RGB'))
 
 try:
     print("Press CTRL-C to stop.")
